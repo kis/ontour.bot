c715082d0a3b06fd574a832dfe81c10654d959fd
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-04-03 12:41:08 +0300

    implement search by locations

--- api.js
+++ api.js
@@ -1,6 +1,18 @@
 const request = require('superagent');
 const API_KEY = 'iOAsnWYdLjjhNvvM';
 
+async function getLocation(location_query) {
+    return request
+        .get('http://api.songkick.com/api/3.0/search/locations.json')
+        .query({ apikey: API_KEY, query: location_query, per_page: 1 });
+}
+
+async function getEventsByMetroAreaID(id) {
+    return request
+        .get(`http://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json`)
+        .query({ apikey: API_KEY, per_page: 5 });
+}
+
 async function getArtist(artist_name) {
     return request
         .get('http://api.songkick.com/api/3.0/search/artists.json')
@@ -15,5 +27,7 @@ async function getEventsByArtist(artist) {
 
 module.exports = {
     getArtist,
-    getEventsByArtist
+    getEventsByArtist,
+    getLocation,
+    getEventsByMetroAreaID
 }
--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { getArtist, getEventsByArtist } = require('./api');
+const { getArtist, getEventsByArtist, getLocation, getEventsByMetroAreaID } = require('./api');
 const TelegramBot = require('node-telegram-bot-api');
 const moment = require('moment');
 const token = process.env.TOKEN;
@@ -16,15 +16,15 @@ else {
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome. To search concerts type /search");
+    bot.sendMessage(msg.chat.id, "Welcome. To search concerts by artists type /artists. To search events in different cities type /locations.");
 });
 
 bot.onText(/\/help/, (msg) => {
     let message = `
 Commands:
 /start
-/locations
-/artists
+/artists - search concerts by artists and bands
+/locations - search events in different cities
 /help
     `;
 
@@ -33,7 +33,7 @@ Commands:
     });
 });
 
-bot.onText(/\/search/, (msg) => {
+bot.onText(/\/artists/, (msg) => {
     bot.sendMessage(msg.chat.id, "Cool, now type the name of the band");
 
     bot.once("message", async reply => {
@@ -61,18 +61,68 @@ bot.onText(/\/search/, (msg) => {
 
         if (!eventsList.event || !eventsList.event.length) return;
 
-        let eventTpl = '';
+        console.log(artist)
+
+        let ontourUntil = moment(artist.onTourUntil).format('MMM D, YYYY');
+        let eventTpl = `<b>${artist.displayName.toUpperCase()} CONCERTS
+ON TOUR UNTIL ${ontourUntil}</b>\n\n`;
 
         eventsList.event.map(event => {
             let datetime = moment(event.start.datetime).format('MMM D, YYYY HH:mma');
 		    let isValid = moment(datetime, 'MMM D, YYYY HH:mma').isValid();
 		    if (!isValid) datetime = moment(event.start.date).format('MMM D, YYYY');
 
-            eventTpl += `
-<b>🎤 ${event.displayName}</b>
+            eventTpl += `<b>🎤 ${event.displayName}</b>
 📌 ${event.location.city}
-🕘 ${datetime}
-            `;
+🕘 ${datetime}\n\n`;
+        });
+
+        bot.sendMessage(msg.chat.id, eventTpl, {
+            "parse_mode": "html"
+        });
+    });
+});
+
+bot.onText(/\/locations/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Cool, now type the city");
+
+    bot.once("message", async reply => {
+        bot.sendMessage(msg.chat.id, `Let's search for events in ${reply.text}...`);
+
+        let cities = await getLocation(reply.text);
+        let citiesParsed = JSON.parse(cities.text);
+
+        if (!citiesParsed || !citiesParsed.resultsPage.results.location) {
+            bot.sendMessage(msg.chat.id, 'Sorry, I haven\'t found such city :(');
+            return;
+        }
+
+        let country = citiesParsed.resultsPage.results.location[0].city.country.displayName;
+        let city = citiesParsed.resultsPage.results.location[0].city.displayName;
+        let metroAreaID = citiesParsed.resultsPage.results.location[0].metroArea.id;
+        let events = await getEventsByMetroAreaID(metroAreaID);
+        let eventsParsed = JSON.parse(events.text);
+        let eventsList = eventsParsed.resultsPage.results;
+        let eventsCount = eventsParsed.resultsPage.totalEntries;
+        
+        let message = !eventsCount ?
+            `Found ${eventsCount} concerts, sorry :(` :
+            `Found ${eventsCount} concerts, I will group it 5 per message, please continue with tap on Next`;
+
+        bot.sendMessage(msg.chat.id, message);
+
+        if (!eventsList.event || !eventsList.event.length) return;
+
+        let eventTpl = `<b>${city.toUpperCase()} ${country.toUpperCase()} CONCERTS</b>\n\n`;
+
+        eventsList.event.map(event => {
+            let datetime = moment(event.start.datetime).format('MMM D, YYYY HH:mma');
+		    let isValid = moment(datetime, 'MMM D, YYYY HH:mma').isValid();
+		    if (!isValid) datetime = moment(event.start.date).format('MMM D, YYYY');
+
+            eventTpl += `<b>🎤 ${event.displayName}</b>
+📌 ${event.location.city}
+🕘 ${datetime}\n\n`;
         });
 
         bot.sendMessage(msg.chat.id, eventTpl, {
