06a7a6afd9b30e080364f1bfc8dabbb049115e2c
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-07-21 22:52:13 +0300

    add sentry error handling

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 
-const bot = require('./bot');
-const artists = require('./bot/artists');
-const locations = require('./bot/locations');
-require('./web')(bot);
+const Raven = require('raven');
+Raven.config('https://3d8641fee61f425c9af50b4c8bbc11ef@sentry.io/1247513').install();
+
+Raven.context(function () {
+    const bot = require('./bot');
+    const artists = require('./bot/artists');
+    const locations = require('./bot/locations');
+    require('./web')(bot);
+});
