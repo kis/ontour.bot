3c43c0459358cfe0bd1c42e66f2c561ad28a0ce5
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-10-26 21:46:53 +0300

    env

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-require("dotenv").config();
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config();
+}
 
 const Raven = require('raven');
 Raven.config('https://3d8641fee61f425c9af50b4c8bbc11ef@sentry.io/1247513').install();
