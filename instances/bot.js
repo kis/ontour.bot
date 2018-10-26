76d7f983464b9d920e63f9feca54fb785860d7d3
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-10-27 00:42:42 +0300

    debug

--- instances/bot.js
+++ instances/bot.js
@@ -4,9 +4,9 @@ const { TELEGRAM_TOKEN, HEROKU_URL } = require('../config/config');
 let bot;
 
 if (process.env.NODE_ENV === 'production') {
-    bot = new TelegramBot(TELEGRAM_TOKEN);
-    bot.setWebHook(HEROKU_URL + bot.token);
-} else {
+    // bot = new TelegramBot(TELEGRAM_TOKEN);
+    // bot.setWebHook(HEROKU_URL + bot.token);
+// } else {
     bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 }
 
