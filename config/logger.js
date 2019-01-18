13e3f2ee8fd34fb6057978ba2ee108396c80a2a7
Kirill Stepkin <kirillstyopkin@gmail.com>
2019-01-18 19:26:10 +0200

    log data to dynamodb

--- config/logger.js
+++ config/logger.js
@@ -1,23 +1,75 @@
+const uuid = require('uuid');
 const { getAnalytics } = require('./analytics');
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
 
 async function log(msg, event, params) {
-    getAnalytics().identify({
-        userId: msg.from.id,
-        traits: {
-            name: [msg.from.first_name, msg.from.last_name].join(' '),
-            username: msg.from.username,
-            createdAt: msg.date
-        }
-    });
+    try {
+        getAnalytics().identify({
+            userId: msg.from.id,
+            traits: {
+                name: [msg.from.first_name, msg.from.last_name].join(' '),
+                username: msg.from.username,
+                createdAt: msg.date
+            }
+        });
+    
+        getAnalytics().track({
+            userId: msg.from.id,
+            event: event,
+            params: params,
+            chat_id: msg.chat.id,
+            message_id: msg.message_id
+        });
+    
+        console.log('log');
 
-    getAnalytics().track({
-        userId: msg.from.id,
-        event: event,
-        params: params,
-        chat_id: msg.chat.id,
-        message_id: msg.message_id
-    });
-} 
+        const item = {
+            TableName: 'ontour_bot_data',
+            Item: {
+                id: {
+                    S: uuid.v1(),
+                },
+                user_id: {
+                    S: String(msg.from.id),
+                },
+                chat_id: {
+                    S: String(msg.chat.id),
+                },
+                message_id: {
+                    S: String(msg.message_id),
+                },
+                name: {
+                    S: [msg.from.first_name, msg.from.last_name].join(' '),
+                },
+                username: {
+                    S: msg.from.username,
+                },
+                createdAt: {
+                    S: String(msg.date),
+                },
+                event: {
+                    S: event,
+                },
+                params: {
+                    S: params,
+                }
+            }
+        };
+    
+        dynamodb.putItem(item, function(err, data) {
+            if (err) {
+                console.log("Error", err);
+            } else {
+                console.log("Success", data);
+            }
+        });
+    } catch(error) {
+        console.log("Error", error);
+    }
+}
 
 module.exports = {
     log
